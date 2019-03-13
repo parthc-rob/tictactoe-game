5b54348ae160ed4fa8617995ec8875cb26d4b6fc
Parth Chopra <parthc@umich.edu>
2019-03-13 11:34:30 -0400

    fix spacing issues

--- include/gameboard.h
+++ include/gameboard.h
@@ -39,14 +39,14 @@ namespace ticTacUtils {
 			= {
 				{player_enum::player_0 	, "Player 0"},
 				{player_enum::player_x 	, "Player X"},
-				{player_enum::none 		, "No One"},
+				{player_enum::none 	, "No One"},
 			};
 	std::map<player_enum, char>
 			player_symbol
 			= {
 				{player_enum::player_0 	, '0'},
 				{player_enum::player_x 	, 'X'},
-				{player_enum::none 		, ' '},
+				{player_enum::none 	, ' '},
 			};
 	typedef std::vector <player_enum>
 					board_type_t;
@@ -66,36 +66,34 @@ namespace ticTacUtils {
 	int 			convertRowColToIndex(int row, int column, int board_size);
 	void			printRowSeparator(int board_size);
 	bool			promptReplay();
-	int				promptBoardSize();
+	int			promptBoardSize();
 }
 
 class GameBoard {
-	int 								board_size;
+	int 						board_size;
 	ticTacUtils::board_type_t 			current_board;
 	ticTacUtils::board_type_t			player_0_board;
 	ticTacUtils::board_type_t			player_X_board;
-	player_enum							who_won;
-	int 								is_game_over;
-	ticTacUtils::line_list_t							line_occupancy;
-	char 								colStart = 'A';
-	char 								no_one = ' ';
-	std::list<ticTacUtils::cell_t>		emptyCells;
-	bool								visualize = true;
-
+	player_enum					who_won;
+	int 						is_game_over;
+	ticTacUtils::line_list_t			line_occupancy;
+	char 						colStart = 'A';
+	std::list<ticTacUtils::cell_t>			emptyCells;
+	bool						visualize = true;
 public:
 	GameBoard(bool vis = true, int board_size = 3);
 	void 						resetBoard();
 	void						resetEmptyCells();
-	player_enum 				checkFilledLine(line_type line);
-	player_enum 				checkFilledLine(line_type line,
-											ticTacUtils::cell_t rowCol);
+	player_enum 					checkFilledLine(line_type line);
+	player_enum 					checkFilledLine(line_type line,
+									ticTacUtils::cell_t rowCol);
 	bool						isBoardFilled();
 	int 						isGameOver();
-	ticTacUtils::cell_t			processKeyboardInput(std::string userInput);
+	ticTacUtils::cell_t				processKeyboardInput(std::string userInput);
 	bool 						playMove(ticTacUtils::cell_t inputCell,
-											bool is_player_0);
+								bool is_player_0);
 	bool 						playMove(std::string userInput,
-											bool is_player_0);
+								bool is_player_0);
 	bool						playRandomMove(bool is_player_0);
 	void						showBoard(bool visualize = false);
 	std::string					whoWon();
